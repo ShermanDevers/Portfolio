b486149b0570d9c06be0e0475c9860e9e4828277
linear0s <67293014+linear0s@users.noreply.github.com>
2022-11-22 21:58:23 -0500

    Create PassPossiblity.py

---
+++ PassPossiblity.py
@@ -0,0 +1,5 @@
+import string
+import random
+poschara = int(input("How many possible characters for each space: "))
+amountofspaces = int(input("How many spaces: "))
+print(f"{poschara ** amountofspaces:,}")
