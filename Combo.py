b95956d50dd2a5bb2d348a9587fdd6d2aeaa99bc
linear0s <67293014+linear0s@users.noreply.github.com>
2022-11-24 18:45:14 -0500

    Update Combo.py

--- Combo.py
+++ Combo.py
@@ -1,16 +1,20 @@
 import string
 import random
+class Gen:
+    def __init__(self,pass_length):
+        self.chara = ''.join(random.choice(string.digits + string.ascii_letters + string.punctuation) for x in range(pass_length)) 
+def write():
+    with open("possible_passwords.txt","a") as writing:
+            writing.write(f"{chara.chara}\n")
 possible_pass = int(input("How many passwords do you want to create?: "))
 ran_or_fixed = str(input("Do you want the length to be random or chosen?: "))
 if ran_or_fixed in ["random","Random"]:
     for x in range(possible_pass):
         pass_length = random.randint(10,20)
-        chara = ''.join(random.choice(string.digits + string.ascii_letters + string.punctuation) for x in range(pass_length))
-        with open("possible_passwords.txt","a") as writing:
-            writing.write(f"{chara}\n")
+        chara = Gen("2",pass_length)
+        write()
 if ran_or_fixed in ["chosen","Chosen"]:
     pass_length = int(input("How long do you want them do be?: "))
-    for x in range(possible_pass):    
-        chara = ''.join(random.choice(string.digits + string.ascii_letters + string.punctuation) for x in range(pass_length))
-        with open("possible_passwords.txt","a") as writing:
-            writing.write(f"{chara}\n")
+    for x in range(possible_pass):
+        chara = Gen("2",pass_length)    
+        write()
