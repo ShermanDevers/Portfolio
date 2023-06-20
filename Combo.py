d0bfdf53b9fbf9da5c8fc2cf3748c1c28da2a432
linear0s <67293014+linear0s@users.noreply.github.com>
2023-06-19 22:51:42 -0500

    Updated formatting and variable names

--- Combo.py
+++ Combo.py
@@ -2,16 +2,16 @@ import string
 import random
 class Gen:
     def __init__(self,pass_length):
-        self.chara = ''.join(random.choice(string.digits + string.ascii_letters + string.punctuation) for x in range(pass_length)) 
+        self.passwd = ''.join(random.choice(string.digits + string.ascii_letters + string.punctuation) for x in range(pass_length)) 
 def write():
     with open("possible_passwords.txt","a") as writing:
-            writing.write(f"{chara.chara}\n")
+            writing.write(f"{passwd.passwd}\n")
 possible_pass = int(input("How many passwords do you want to create?: "))
 ran_or_fixed = str(input("Do you want the length to be random or chosen?: "))
 if ran_or_fixed in ["random","Random"]:
     for x in range(possible_pass):
         pass_length = random.randint(10,20)
-        chara = Gen(pass_length)
+        passwd = Gen(pass_length)
         write()
 if ran_or_fixed in ["chosen","Chosen"]:
     pass_length = int(input("How long do you want them do be?: "))
