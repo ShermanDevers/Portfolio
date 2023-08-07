91e5acf31c7695f1b33ed916fc1ae8d755a067f3
linear0s <67293014+linear0s@users.noreply.github.com>
2023-08-07 15:23:01 -0500

    Update YoutubeDownloader.py
    Added arguments

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,44 +1,45 @@
 from pytube import YouTube, Playlist
-import time, requests
+import requests
+import argparse
 
+def download_vids(vid_list):
+    for link in vid_list:
+        yt = YouTube(link)
+        yt.streams.get_highest_resolution().download("/home/sherman/Videos")
+        print(f"Successfully downloaded: '{yt.title}'.mp4")
 
-def main():
-
-    vop = str(input("Videos or Playlist> "))
-    if vop in ["Playlist", "playlist"]:
-
-        playlist_link = input("Link to the playlist> ")
-        ytplist = []
-        ytplaylist = Playlist(playlist_link)
-        for x in ytplaylist.video_urls:
-            ytplist.append(x)
-        try:
-            vidnum = 0
-            for video in ytplist:
-                ytp = YouTube(video)
-                ytp.streams.get_highest_resolution().download("/home/sherman/Videos")
-                print(f"Successfully downloaded {1+vidnum}: {ytp.title}.mp4")
-                vidnum += 1
-        except requests.HTTPError as httperror:
-            print(f"Server overload {httperror}")
-        time.sleep(5)
-
-    elif vop in ["Videos", "videos", "Video", "video"]:
-
-        linknum = int(input("How many vids do you want to download> "))
-        testlist = []
-        for i in range(linknum):
-            url = input("Link > ")
-            testlist.append(url)
-        for x in testlist:
-            yt = YouTube(x)
-            yt.streams.get_highest_resolution().download("/home/sherman/Videos")
-            print(f"Successfully downloaded: {yt.title}.mp4")
-        time.sleep(5)
-
-    else:
-        print("Not an option")
+def download_playlist(playlist):
+    try:
+        vidnum = 0
+        for video in playlist:
+            ytp = YouTube(video)
+            ytp.streams.get_highest_resolution().download("/home/sherman/Videos")
+            vidnum += 1
+            print(f"Successfully downloaded {vidnum}: {ytp.title}.mp4")
+    except requests.HTTPError as httperror:
+        print(f"Server overload {httperror}")
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser(description="CLI program to download YouTube videos and Playlists")
+    parser.add_argument("Video_or_Playlist",type=str,help="Choose whether to download a Video or a Playlist")
+    parser.add_argument("videos",nargs="+",type=str,help="URLs of the videos")
+    args = parser.parse_args()
+    pass
+    if args.Video_or_Playlist in ["Playlist", "playlist"]:
+
+        playlist_link = input("Link to the playlist> ")
+        yt_playlist = []
+        ytplaylist = Playlist(playlist_link)
+        for x in ytplaylist.video_urls:
+            yt_playlist.append(x)
+            download_playlist(yt_playlist)
+
+    elif args.Video_or_Playlist in ["Videos", "videos", "Video", "video"]:
+
+        
+        download_vids(args.videos)
+
+    else:
+        print("Not an option")
