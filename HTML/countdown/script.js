d4938b5ffc7307a48cfce88a4781557a6bee9859
Linear0s <shermandeverslll@gmail.com>
2023-12-19 12:20:59 -0600

    Added hours,minutes, and seconds as separate input

--- HTML/countdown/script.js
+++ HTML/countdown/script.js
@@ -1,16 +1,20 @@
 let timerStart = false;
+let hours;
+let minutes;
+let seconds;
 let secondsRemaining;
-secondsRemaining = parseInt(document.querySelector('#seconds').value);        
+
+hours = parseInt(document.querySelector('#hours').value);
+minutes = parseInt(document.querySelector('#minutes').value);
+seconds = parseInt(document.querySelector('#seconds').value);
+secondsRemaining = (hours * 3600) + (minutes * 60) + seconds;
+
 const button = document.querySelector('button');
 console.log("Script running")
 button.addEventListener('click', updateTimer);
 
 function start() {
     const timerElement = document.querySelector('.timer');
-    secondsRemaining = document.querySelector('#seconds').value;
-    const hours = Math.floor(secondsRemaining / 3600);
-    const minutes = Math.floor((secondsRemaining / 60) % 60);
-    const seconds = secondsRemaining % 60;
     timerElement.textContent = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
     timerStart = true;
 }
@@ -24,7 +28,6 @@ function updateTimer() {
         console.log(secondsRemaining);
         if (secondsRemaining === 0) {
             timerElement.textContent = "Time's up!";
-            // add any additional logic for timer completion here
             return;
         }
         const hours = Math.floor(secondsRemaining / 3600);
@@ -40,10 +43,6 @@ updateTimer();
 
 function resetTimer() {
     const timerElement = document.querySelector('.timer');
-    secondsRemaining = document.querySelector('#seconds').value;
-    const hours = Math.floor(secondsRemaining / 3600);
-    const minutes = Math.floor((secondsRemaining / 60) % 60);
-    const seconds = secondsRemaining % 60;
     timerElement.textContent = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
     timerStart = false;
 }
