4dc543404bec999d0fcd6d5f44fb0968ccd75330
Linear0s <shermandeverslll@gmail.com>
2023-12-31 13:54:56 -0600

    fixed variable names

--- Python/Turn based game.py
+++ Python/Turn based game.py
@@ -10,7 +10,7 @@ opp_move_damage = {"Fireball": 40, "Fire wall": 30}
 moves = {"Offense": {"Water Bullet": 40}, "Defense": {"Water Prism": 30}}
 
 
-def Players_turn(opponent_health, defense, opponent_defense):
+def players_turn(opponent_health, defense, opponent_defense):
     print("\nPlayer's turn")
     print(f"Available moves")
     print(f"Offense: {moves['Offense']}")
@@ -32,7 +32,7 @@ def Players_turn(opponent_health, defense, opponent_defense):
     return "Bot"
 
 
-def Bots_turn(player_health, defense, opponent_defense):
+def bots_turn(player_health, defense, opponent_defense):
     print("Bots Turn")
     time.sleep(0.5)
     random_move = random.sample(opp_moves, 1)
@@ -59,20 +59,25 @@ confirm = input("Do you want to play a game?: ")
 if confirm in ["Y", "y", "yes"]:
     first_turn = input("Who goes first player or bot?: ")
     if first_turn in ["player"]:
-        next_turn = Players_turn(
+        next_turn = players_turn(
             opponent["Health"], player["Defense"], opponent["Defense"]
         )
 
     if first_turn in ["bot"]:
-        Bots_turn(player["Health"], opponent["Defense"])
+        bots_turn(player["Health"], player["Defense"], opponent["Defense"])
 
 
-while player["Health"] > 0 or opponent["Health"] > 0:
-    if next_turn == "Player":
-        next_turn = Players_turn(
-            opponent["Health"], player["Defense"], opponent["Defense"]
-        )
-    if next_turn == "Bot":
-        next_turn = Bots_turn(
-            player["Health"], player["Defense"], opponent["Defense"]
-        )
+def main():
+    while player["Health"] > 0 or opponent["Health"] > 0:
+        if next_turn == "Player":
+            next_turn = players_turn(
+                opponent["Health"], player["Defense"], opponent["Defense"]
+            )
+        if next_turn == "Bot":
+            next_turn = bots_turn(
+                player["Health"], player["Defense"], opponent["Defense"]
+            )
+
+
+if __name__ == "__main__":
+    main()
