df01c09c8dd2cb7cb4f6452d05e1cc954160c88f
Linear0s <shermandeverslll@gmail.com>
2024-01-07 15:50:32 -0600

    Changed simplewebscraper.py to have a class

--- Python/simplewebscraper.py
+++ Python/simplewebscraper.py
@@ -1,21 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Scrape:
+    def __init__(self,url,file_name):
+        self.url = url
+        self.file_name = file_name
+
+    def scrape(url,file_name):
+        content = requests.get(url)
+
+        with open(file_name, "wb") as pysitew:
+
+            contents = BeautifulSoup(content.text,"html.parser")
+            content_pretty = contents.prettify()
+            pysitew.write(bytes(content_pretty, "utf-8"))
+            print("File Created")
+
+
+
 url = input("Input Url to scrape> ")
 file_name = input("File Name to put html content in: ")
-content = requests.get(url)
 
-with open(file_name, "wb") as pysitew:
-
-    contents = BeautifulSoup(content.text,"html.parser")
-    content_pretty = contents.prettify()
-    pysitew.write(bytes(content_pretty, "utf-8"))
-    print("File Created")
-    
-links = [link.get("href") for link in contents.find_all("link")]
-
-for link in links:
-    if link is None:
-        continue
-    if "/s" in link:
-        print(link)
+Scrape.scrape(url,file_name)
