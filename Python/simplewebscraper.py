8c9aea69e9698e860aa04629de85371644b9b757
Linear0s <shermandeverslll@gmail.com>
2024-01-06 19:41:41 -0600

    Added the option is make a custom file name

--- Python/simplewebscraper.py
+++ Python/simplewebscraper.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 url = input("Input Url to scrape> ")
-
+file_name = input("File Name to put html content in: ")
 content = requests.get(url)
 
-with open("scraped_website.html", "wb") as pysitew:
+with open(file_name, "wb") as pysitew:
 
     contents = BeautifulSoup(content.text,"html.parser")
     content_pretty = contents.prettify()
     pysitew.write(bytes(content_pretty, "utf-8"))
+    print("File Created")
     
 links = [link.get("href") for link in contents.find_all("link")]
 
