d40e7cbc855ccd23b84a51dcf2ac729d68d6fde4
Sherman Devers <splitsonmyd@gmail.com>
2024-04-14 17:13:17 -0500

    Removed unnecessary things

--- Python/YouTubeDownloader/YoutubeDownloader.py
+++ Python/YouTubeDownloader/YoutubeDownloader.py
@@ -1,16 +1,13 @@
 from pytube import YouTube, Playlist, exceptions
+from pytube.cli import on_progress
 import requests
-import argparse
-import time
-from threading import Thread
 
 
 def download_vids(vid_list, dl_location):
     print("Downloading Video(s)")
     for link in vid_list:
-        yt = YouTube(link)
+        yt = YouTube(link, on_progress_callback=on_progress)
         yt.streams.get_highest_resolution().download(dl_location)
-        print()
         print(f"Successfully downloaded: '{yt.title}'.mp4")
 
 
@@ -19,7 +16,6 @@ def download_playlist(playlist_link, dl_location):
     for video in playlist.videos:
         try:
             video.streams.get_highest_resolution().download(dl_location)
-            progress_bar()
         except exceptions.AgeRestrictedError:
             print(f"{video.title}: Age Restricted")
         except requests.HTTPError as httpe:
@@ -31,11 +27,11 @@ if __name__ == "__main__":
     video_or_playlist = input("Video(s) or Playlist?: ")
     download_location = input("Where do you want to store the video?: ")
 
-    if Video_or_Playlist in ["Playlist", "playlist"]:
+    if video_or_playlist in ["Playlist", "playlist"]:
         link = input("Link of the playlist> ")
         download_playlist(link, download_location)
 
-    elif Video_or_Playlist in ["Videos", "videos", "Video", "video"]:
+    elif video_or_playlist in ["Videos", "videos", "Video", "video"]:
         video_amount = int(input("Amount of videos> "))
         video_list = []
 
