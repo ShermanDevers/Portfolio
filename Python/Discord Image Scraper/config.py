96c9a280d1260b14a9224be0b18bd55fb1c3ca18
Sherman Devers <67293014+ShermanDevers@users.noreply.github.com>
2025-03-21 18:42:58 +0000

    Made token names more appropriate

--- Python/Discord Image Scraper/config.py
+++ Python/Discord Image Scraper/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_API_TOKEN = os.getenv("DISCORD_API_T")
-AUTH = os.getenv("AUTH_T")
+BOT_API_TOKEN = os.getenv("DISCORD_API_T")
+DISCORD_API_AUTH = os.getenv("AUTH_T")
